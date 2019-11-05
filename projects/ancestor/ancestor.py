fe576bb630eeaa24bab390cd77b66e71fd732841
ken1286 <kindle1286@gmail.com>
2019-11-05 11:02:09 -0800

    day 1 and day 2 MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,61 @@
+from util import Graph, Stack, Queue
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # vertices: digits
+    #
+    anGraph = Graph()
+
+    for item in ancestors:
+        anGraph.add_vertex(item[0])
+        anGraph.add_vertex(item[1])
+
+    for item in ancestors:
+        # anGraph.add_edge(item[0], item[1])
+        # anGraph.add_edge(item[1], item[0])
+        # anGraph.add_bidirectional_edge(item[0], item[1])
+        anGraph.add_edge(item[1], item[0])
+
+    stack = Stack()
+    visited = set()
+    stack.push([starting_node])
+    paths = []
+
+    print(anGraph.vertices)
+
+    while stack.size() > 0:
+        path = stack.pop()
+        vertex = path[-1]
+        if vertex not in visited:
+            paths.append(path)
+            print(paths)
+            visited.add(vertex)
+            for next_vert in anGraph.vertices[vertex]:
+                # if vertex > 9 and next_vert > vertex:
+                #     new_path = list(path)
+                #     new_path.append(next_vert)
+                #     stack.push(new_path)
+                #     continue
+
+                if vertex < 10 and (next_vert < vertex or next_vert > 9):
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    stack.push(new_path)
+
+    if len(paths) == 1:
+        return -1
+
+    if len(paths[-1]) == len(paths[-2]):
+        print(paths[-2][-1])
+        return paths[-2][-1]
+    else:
+        print(paths[-1][-1])
+        return paths[-1][-1]
+
+
+an_test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+           (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+
+earliest_ancestor(an_test, 10)
+print(earliest_ancestor(an_test, 3))
--- projects/ancestor/util.py
+++ projects/ancestor/util.py
@@ -1,7 +1,35 @@
-"""
-Simple graph implementation
-"""
-from util import Stack, Queue  # These may come in handy
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
 
 
 class Graph:
@@ -22,6 +50,16 @@ class Graph:
         """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Cannot create edge based on given vertices")
+
+    def add_bidirectional_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
         else:
             raise IndexError("Cannot create edge based on given vertices")
 
@@ -129,84 +167,3 @@ class Graph:
                     new_path = list(path)
                     new_path.append(next_vert)
                     stack.push(new_path)
-
-
-if __name__ == '__main__':
-    graph = Graph()  # Instantiate your graph
-    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
-    graph.add_vertex(1)
-    graph.add_vertex(2)
-    graph.add_vertex(3)
-    graph.add_vertex(4)
-    graph.add_vertex(5)
-    graph.add_vertex(6)
-    graph.add_vertex(7)
-    graph.add_edge(5, 3)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 1)
-    graph.add_edge(4, 7)
-    graph.add_edge(1, 2)
-    graph.add_edge(7, 6)
-    graph.add_edge(2, 4)
-    graph.add_edge(3, 5)
-    graph.add_edge(2, 3)
-    graph.add_edge(4, 6)
-
-    '''
-    Should print:
-        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    '''
-    print(graph.vertices)
-
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    print('starting DFT')
-    graph.dft(1)
-
-    '''
-    Valid BFT paths:
-        1, 2, 3, 4, 5, 6, 7
-        1, 2, 3, 4, 5, 7, 6
-        1, 2, 3, 4, 6, 7, 5
-        1, 2, 3, 4, 6, 5, 7
-        1, 2, 3, 4, 7, 6, 5
-        1, 2, 3, 4, 7, 5, 6
-        1, 2, 4, 3, 5, 6, 7
-        1, 2, 4, 3, 5, 7, 6
-        1, 2, 4, 3, 6, 7, 5
-        1, 2, 4, 3, 6, 5, 7
-        1, 2, 4, 3, 7, 6, 5
-        1, 2, 4, 3, 7, 5, 6
-    '''
-    print('starting BFT')
-    graph.bft(1)
-
-    '''
-    Valid DFT recursive paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    print('DFT recursive start')
-    graph.dft_recursive(1)
-
-    '''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-    print('BFS start')
-    print(graph.bfs(1, 6))
-
-    '''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-    print('DFS start')
-    print(graph.dfs(1, 6))
